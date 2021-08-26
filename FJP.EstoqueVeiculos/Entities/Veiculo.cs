5c5a2f5875114bbe7480f457f6ea31b46002e239
Fernando J Prado <f.j.prado@hotmail.com>
2021-08-26 17:19:56 -0300

    Contrato repositório

---
+++ FJP.EstoqueVeiculos/Entities/Veiculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FJP.EstoqueVeiculos.Entities
+{
+    public class Veiculo
+    {
+        public Guid Id { get; set; }
+        public string Chassi { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public int AnoFabricacao { get; set; }
+        public int AnoModelo { get; set; }
+        public decimal? ValorVenda { get; set; }
+        public DateTime? DataVenda { get; set; }
+        public decimal ValorCompra { get; set; }
+        public DateTime DataCompra { get; set; }
+    }
+}
---
+++ FJP.EstoqueVeiculos/Repositories/IVeiculoRepository.cs
@@ -0,0 +1,19 @@
+﻿using FJP.EstoqueVeiculos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FJP.EstoqueVeiculos.Repositories
+{
+    public interface IVeiculoRepository
+    {
+        Task<List<Veiculo>> Obter(int pagina, int quantidade);
+        Task<Veiculo> Obter(Guid id);
+        Task<List<Veiculo>> Obter(string modelo, string marca);
+        Task Inserir(Veiculo veiculo);
+        Task Atualizar(Veiculo veiculo);
+        Task Remover(Guid id);
+        Task Vender(Veiculo veiculo);
+    }
+}
